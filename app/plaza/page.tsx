df24a4c011d69b31328e605a4b4d882c4d1e356e
report11071216-ux <report11071216@gmail.com>
2026-05-26 10:21:32 +0900

    Update page.tsx

--- app/plaza/page.tsx
+++ app/plaza/page.tsx
@@ -225,10 +225,10 @@ export default async function PlazaPage() {
         <TopRankCompact guilds={topRankings} />
 
         <div className="grid grid-cols-1 lg:grid-cols-12 gap-6">
-          <div className="lg:col-span-8">
+          <div className="lg:col-span-9">
             <BoardPreview posts={plazaPosts} />
           </div>
-          <aside className="lg:col-span-4 space-y-6">
+          <aside className="lg:col-span-3 space-y-6">
             <MyProfileCard
               isLoggedIn={!!user}
               profile={myProfile}
